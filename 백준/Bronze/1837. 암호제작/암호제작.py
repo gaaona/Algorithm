9dfec09c9c0560963e41e5f4e1712c28b31ab297
rinkoko <gnaao1005@gmail.com>
2025-02-09 19:13:58 +0900

    [Bronze III] Title: 암호제작, Time: 176 ms, Memory: 43916 KB -BaekjoonHub

--- 백준/Bronze/1837. 암호제작/암호제작.py
+++ 백준/Bronze/1837. 암호제작/암호제작.py
@@ -1,9 +1,21 @@
+def get_primes(limit):
+    primes = []
+    sieve = [True] * (limit + 1)
+    
+    for i in range(2, limit):
+        if sieve[i]:  
+            primes.append(i)
+            for j in range(i * i, limit, i):  
+                sieve[j] = False
+                
+    return primes  
+
 def is_divided_under(num1, num2):
-    for i in range(2, num2+1):
-        if i == num2:
-            return f'GOOD'
-        if num1%i == 0:
-            return f'BAD {i}'
+    primes = get_primes(num2)  # K보다 작은 소수 리스트 생성
+    for p in primes:
+        if num1 % p == 0:
+            return f'BAD {p}'
+    return "GOOD"
 
 code, r = map(int, input().split())
 print(is_divided_under(code, r))
