413f0361ebcf30956343995f8367aff9a5229bcb
rinkoko <gnaao1005@gmail.com>
2025-08-19 08:36:03 +0900

    [Silver II] Title: 최대 힙, Time: 1424 ms, Memory: 115464 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+import java.util.Scanner;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < N;i++) {
+            int x = sc.nextInt();
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+    }
+}
