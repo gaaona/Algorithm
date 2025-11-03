34b8e7f691b13c05ffd6c00b2877bfced016e936
rinkoko <gnaao1005@gmail.com>
2025-11-04 08:39:17 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 44.35 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+let answer = '';
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    for (let i = 0; i < str.length; i++){
+        if (str.charCodeAt(i) > 96){
+            answer += str[i].toUpperCase();
+        } else {
+            answer += str[i].toLowerCase(); 
+        }
+    } 
+    
+    console.log(answer)
+});
