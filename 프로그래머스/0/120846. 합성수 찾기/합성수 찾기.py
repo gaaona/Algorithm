180e5021eee58f454fe0a81973f439896f1eb0b2
rinkoko <gnaao1005@gmail.com>
2026-01-06 13:43:46 +0900

    [level 0] Title: 합성수 찾기, Time: 0.07 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,19 +1,9 @@
-dividers = [False] * 101
-
-for i in range(4, 101):
-    for j in range(2,i//2+1):
-        if i%j==0:
-            dividers[i] = True
-            break
-    
-
-
 def solution(n):
-    global dividers
     answer = 0
-    
-    for i in range(n+1):
-        if dividers[i]:
-            answer += 1
-            
+    for i in range(4, n + 1):
+        # 약수가 하나라도 존재하면 (합성수라면) 카운트
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                answer += 1
+                break
     return answer
