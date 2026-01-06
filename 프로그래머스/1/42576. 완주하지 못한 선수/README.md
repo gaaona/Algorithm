d566388dfbe3a363b867c19fcbb484261b3653c2
rinkoko <gnaao1005@gmail.com>
2026-01-06 15:50:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.30 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576#) 
 
 ### 성능 요약
 
-메모리: 25.4 MB, 시간: 85.09 ms
+메모리: 38 MB, 시간: 76.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 06일 15:45:36
+2026년 01월 06일 15:50:13
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,8 @@
+from collections import Counter
+
 def solution(participant, completion):
-    answer = ''
-
-    participant.sort()
-    completion.sort()
-    N = len(participant)
+    # 각 명단의 개수를 센 뒤 차집합을 구함
+    answer = Counter(participant) - Counter(completion)
     
-    for i in range(N):
-        if i == N-1:
-            answer = participant[-1]
-            break
-            
-        if participant[i] != completion[i]:
-            answer = participant[i]
-            break
-
-    return answer
+    # 결과는 {'mislav': 1} 형태이므로 key값만 추출
+    return list(answer.keys())[0]
